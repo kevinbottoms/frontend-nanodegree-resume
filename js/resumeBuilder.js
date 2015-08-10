bbd38f651e9b78e4e897a980fac5061a3efc8034
Kevin Bottoms <webdev@kevinbottoms.com>
2015-08-10 22:04:00 +0200

    feat: Add name and role using javascript

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var formattedName = HTMLheaderName.replace("%data%", "Kevin Bottoms");
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+
+
+$("#header").append(formattedRole);
+$("#header").prepend(formattedName);
