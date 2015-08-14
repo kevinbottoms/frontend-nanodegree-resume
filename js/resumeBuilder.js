d352fb56017c4a3a914e384110a4e98121b1788e
kevinbottoms <webdev@kevinbottoms.com>
2015-08-14 16:00:33 +0200

    feat: Clean up resumeBuilder.js to JSON

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,16 +2,78 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- var name = "Kevin Bottoms";
- var role = "Web Developer";
+var bio = {
+  "name" : "Kevin Bottoms",
+  "role" : "Web Developer",
+  "contacts" : {
+    "mobile" : "015119054715",
+    "email" : "webdev@kevinbottoms.com",
+    "github" : "kevinbottoms",
+    "twitter" : "NA",
+    "location" : "Germany"
+  },
+  "welcomeMessage" : "We got to move these color TV's",
+  "skills" : [
+    "HTML5", "CSS3", "JavaScript", "Responsive Design"
+  ],
+  "bioPic" : "images/me.jpg"
+}
 
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+var education = {
+  "schools" : [
+    {
+      "name" : "University of Maryland University College",
+      "city" : "Wiesbaden",
+      "degree" : "Masters",
+      "major" : "Software Engineering",
+      "dates" : 2015,
+      "url" : "http://umuc.edu"
+    },
+    {
+      "name" : "University of Maryland University College",
+      "city" : "Wiesbaden",
+      "degree" : "BS",
+      "major" : "Digital Media and Web Technology",
+      "dates" : 2014,
+      "url" : "http://umuc.edu"
+    }
+  ],
+  "onlineCourses" : [
+    {
+      "title" : "Frontend Web Developer Course",
+      "school" : "Udacity",
+      "dates" : 2015,
+      "url" : "http://udacity.com"
+    }
+  ]
+}
 
+var work = {
+  "jobs" : [
+    {
+      "employer" : "UMUC",
+      "title" : "Senior Military Education Coordinator",
+      "dates" : "December 2004 - Present",
+      "description" : "Helped manage employees with the goal of facilitating higher education in military communities. Coordinated class schedules, staff and resources to ensure success in a deadline and result oriented environment."
+    },
+    {
+      "employer" : "AAFES",
+      "title" : "Accounting Associate",
+      "dates" : "December 2003 - December 2004",
+      "description" : "Responsible for the recording and tracking of financial transactions for multiple exchange facilities. Handled sensitive data and ensured financial policies were adhered to while also inspecting possible fraud, waste and abuse."
+    }
+  ]
+}
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
-var s = "audacity";
-s = s[1].toUpperCase() + s.slice(2);
-console.log(s);
+var projects = {
+  "projects" : [
+    {
+      "title" : "Sample Project 1",
+      "dates" : "2015",
+      "description" : "A prototype of a responsive web site utilizing the Bootstrap framework that shows a proficiency in responsive web site design.",
+      "images" : [
+        "images/me.jpg"
+      ]
+    }
+  ]
+}
